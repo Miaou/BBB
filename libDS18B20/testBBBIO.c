dac9e8429aafb83b6f640c0cce5b01cad3c4354c
Miaou on BBB <pa.brameret@gmail.com>
2014-11-30 18:48:59 +0100

    MàJ du test de la lib pour chrono plus de trucs
    Le truc c'est que la lib est config en mode debug, et on a des printf()
    qui prenne beaucoup de temps et empèchent de faire l'opération de changement
    de input à output (ou inversement) rapidement.

--- libDS18B20/testBBBIO.c
+++ libDS18B20/testBBBIO.c
@@ -8,6 +8,7 @@
 
 #define N_TESTS 10
 #define PIN 9,13
+#define N_LOOP 104857600
 
 
 float diff(struct timespec *t0, struct timespec *t1)
@@ -25,9 +26,20 @@ int main(void)
     int i;
 
     iolib_init();
+    
+    // On veut temps mini, on fait fonctionner le proco pour qu'il sorte de som mode d'économie
+    // On en profite pour voir le temps qu'il met à compter, et le temps qu'il met à regarder le temps
+    printf("\nTemps en secondes.\n\nBoucle à %u: ", N_LOOP);
+    clock_gettime(CLOCK_REALTIME, &t[0]);
+    for(i=0; i<N_LOOP; ++i);
+    clock_gettime(CLOCK_REALTIME, &t[1]);
+    clock_gettime(CLOCK_REALTIME, &t[2]);
+    printf("%g\n2 appels conséc: %g\n\n", diff(&t[0],&t[1]), diff(&t[1],&t[2]));
+
     iolib_setdir(PIN, BBBIO_DIR_OUT);
     pin_low(PIN);
     
+    printf("Montées/descentes\n");
     for(i=0; i<N_TESTS; ++i)
     {
         clock_gettime(CLOCK_REALTIME, &t[2*i]);
@@ -37,12 +49,45 @@ int main(void)
     }
 
     pin_low(PIN);
-    iolib_free();
-
+    
+    for(i=0; i<N_TESTS; ++i)
+        printf("-> %g\n",diff(&t[2*i],&t[2*i+1]));
+    
+    printf("\nChangements de directions\n");
     for(i=0; i<N_TESTS; ++i)
     {
-        printf("-> %g\n",diff(&t[2*i],&t[2*i+1]));
+        clock_gettime(CLOCK_REALTIME, &t[2*i]);
+        iolib_setdir(PIN, BBBIO_DIR_OUT);
+        iolib_setdir(PIN, BBBIO_DIR_IN);
+        clock_gettime(CLOCK_REALTIME, &t[2*i+1]);
     }
-
+    
+    for(i=0; i<N_TESTS; ++i)
+        printf("-> %g\n",diff(&t[2*i],&t[2*i+1]));
+    
+    printf("\nLecture de la valeur du signal (haut)\n");
+    for(i=0; i<N_TESTS; ++i)
+    {
+        clock_gettime(CLOCK_REALTIME, &t[2*i]);
+        is_high(PIN);
+        clock_gettime(CLOCK_REALTIME, &t[2*i+1]);
+    }
+    
+    for(i=0; i<N_TESTS; ++i)
+        printf("-> %g\n",diff(&t[2*i],&t[2*i+1]));
+    
+    printf("\nLecture de la valeur du signal (low)\n");
+    for(i=0; i<N_TESTS; ++i)
+    {
+        clock_gettime(CLOCK_REALTIME, &t[2*i]);
+        is_low(PIN);
+        clock_gettime(CLOCK_REALTIME, &t[2*i+1]);
+    }
+    
+    for(i=0; i<N_TESTS; ++i)
+        printf("-> %g\n",diff(&t[2*i],&t[2*i+1]));
+    
+    
+    iolib_free();
     return 0;
 }
