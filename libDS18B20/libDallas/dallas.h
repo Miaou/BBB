db0e44784118492c572b8e50043e160a95c91b91
Chairn <chairn.nq@hotmail.fr>
2014-12-18 17:07:53 +0100

    Ajout de la struct OneWire pour le strong pullup

--- libDS18B20/libDallas/dallas.h
+++ libDS18B20/libDallas/dallas.h
@@ -21,11 +21,16 @@
 #define RECALL_E         0xB8
 #define READ_POWER_SUPPLY 0xB4
 
-/*What's best? enumeration or definition?
-enum Operation {
-    SEARCH_ROM = 0xF0,
-    READ_ROM    = 0x33,
-};*/
+typedef struct OneWire OneWire;
+/* Represents a OneWire pin with a strong pullup pin
+because 3.3V is not enough, we use another pin as strong pullup*/
+struct OneWire
+{
+    char port;
+    char pin;
+    char pullup_port;
+    char pullup_pin;
+};
 
 /*Initialization and termination*/
 LIBDALLAS_API int dallas_init(void);
@@ -33,24 +38,24 @@ LIBDALLAS_API int dallas_free(void);
 
 
 /*Communication : Writing and reading data*/
-LIBDALLAS_API int pulseInit(char port, char pin);
-LIBDALLAS_API int write_bit(char port, char pin, char bit);
-LIBDALLAS_API int read_bit(char port, char pin);
-LIBDALLAS_API int write_byte(char port, char pin, unsigned char by);
-LIBDALLAS_API int read_byte(char port, char pin);
+LIBDALLAS_API int pulseInit(OneWire* onewire);
+LIBDALLAS_API int write_bit(OneWire* onewire, char bit);
+LIBDALLAS_API int read_bit(OneWire* onewire);
+LIBDALLAS_API int write_byte(OneWire* onewire, unsigned char by);
+LIBDALLAS_API int read_byte(OneWire* onewire);
 
 
 /*ROM commands */
 typedef void (*SEARCH_CALLBACK)(unsigned char *);
-LIBDALLAS_API int dallas_rom_read(char port, char pin, unsigned char *bRom);
-LIBDALLAS_API int dallas_rom_search(char port, char pin, SEARCH_CALLBACK found_rom);
-LIBDALLAS_API int dallas_rom_skip(char port, char pin, unsigned char operation);
-LIBDALLAS_API int dallas_rom_match(char port, char pin, unsigned char* rom_code);
+LIBDALLAS_API int dallas_rom_read(OneWire* onewire, unsigned char *bRom);
+LIBDALLAS_API int dallas_rom_search(OneWire* onewire, SEARCH_CALLBACK found_rom);
+LIBDALLAS_API int dallas_rom_skip(OneWire* onewire, unsigned char operation);
+LIBDALLAS_API int dallas_rom_match(OneWire* onewire, unsigned char* rom_code);
 
 /*DS18B20 function commands*/
-LIBDALLAS_API int dallas_scratchpad_write(char port, char pin, unsigned char *data);
-LIBDALLAS_API int dallas_scratchpad_read(char port, char pin, unsigned char *scratch, char num);
-LIBDALLAS_API int dallas_temperature_read(char port, char pin);
+LIBDALLAS_API int dallas_scratchpad_write(OneWire* onewire, unsigned char *data);
+LIBDALLAS_API int dallas_scratchpad_read(OneWire* onewire, unsigned char *scratch, char num);
+LIBDALLAS_API int dallas_temperature_read(OneWire* onewire);
 
 
 #endif
