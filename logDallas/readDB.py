2b237869f9c3b2f75b6851572f9707e272c8aa72
Miaou <miaou.pbl@gmail.com>
2015-01-19 00:02:58 +0100

    Vers un meilleur DAO pour logThermo...

--- logDallas/readDB.py
+++ logDallas/readDB.py
@@ -6,18 +6,36 @@ import sys
 sys.path.append('../libDS18B20/libDallas-PRU')
 sys.path.append('../thermoLog')
 
-from dao import DAO
+from dao import DAO, DAODrawer
+
+try:
+    print('Import pylab+numpy...', end='')
+    import pylab
+    import numpy as np
+    print(' Ok')
+except ImportError:
+    print(' No pylab')
+    pylab = None
+    np = None
 
 
 sDB = 'cristal.db'
 
 
-if __name__=='__main__':
-    dao = DAO(sDB)
-    dao.displaySensors()
-    dao.displayWaves()
-    print('for t,v in dao.iterMeasures(sensID, waveID)')
 
+
+if __name__=='__main__':
+    if not pylab:
+        dao = DAO(sDB)
+        dao.displaySensors()
+        dao.displayWaves()
+        print('for t,v in dao.iterMeasures(sensID, waveID)')
+    else:
+        dao = DAODrawer(sDB)
+        dao.assertPylab()
+        dao.displaySensors()
+        dao.displayWaves()
+        dao.pylabPltWave(3, [], 1300)
 
 
 
