887c2b9decfac4d18142162378f4aafac0b4f19c
Miaou <miaou.pbl@gmail.com>
2015-08-19 00:00:20 +0200

    Calib: gets a curses ui

--- libServo/calibXBox.py
+++ libServo/calibXBox.py
@@ -8,6 +8,7 @@ from evdev import InputDevice, ecodes
 from config import lServos
 from servos import PruInterface, ServoController
 import time
+import curses
 
 
 # InputDevice has a nice active_keys() which avoids the parsing of the events, but not abs_values()...
@@ -48,26 +49,13 @@ def testRawCapa():
 
 
 
-def xpadMenu(dev):
-    def printMenu():
-        print('### Menu ###')
-        print('  Start: quits')
-        print('  Trigger right/left: next/prev leg')
-        print('  A: cycles joints')
-        print('  B: cycle leg positions (femur 90°, tibia 0°, tibia 180°, femur -90°)')
-        print('  Left Stick vertical: adjust min time')
-        print('  Right Stick vertical: adjust max time')
-        print('  Right Stick horizontal + right trigger axis pushed: adjust shift')
-        print('  Y: Show current calibration')
-        print('  Left trigger axis: release to cancel control of the leg')
-        print('  X: this help')
-        print('### Calib order ###')
-        print(' - Adjust femur up to be vertical')
-        print(' - Adjust femur down to be vertical')
-        print(' - Adjust tibia no shift unfolded (horizontal flat leg)')
-        print(' - Adjust tibia no shift folded')
-        print(' - Adjust tibia shift')
-    printMenu()
+def wrap_curses(func):
+    def wrap(*args, **kwargs):
+        return curses.wrapper(func, *args, **kwargs)
+    return wrap
+
+@wrap_curses
+def xpadMenu(stdscr, dev):
     class Status:
         def __init__(self):
             self.iCurLeg = -1
@@ -81,32 +69,18 @@ def xpadMenu(dev):
             self.nextLeg()
         def nextLeg(self, iShift=1):
             self.iCurLeg = (self.iCurLeg+iShift)%6
-            print('Leg: current {} ({})'.format(self.iCurLeg, ['rear right', 'rear left', 'middle right', 'middle left', 'front right', 'front left'][self.iCurLeg]))
             self.lCurServos = lServos[self.iCurLeg*3:self.iCurLeg*3+3] # GLOBAL VAR
             self.iCurJoint = -1
             self.iCurPosition = -1
             self.nextPosition()
             self.nextJoint()
         def nextJoint(self):
-            if self.iCurJoint > -1:
-                print('Joint: LAST {} calib {}'.format(['hip','femur','tibia'][self.iCurJoint], self.tCurCalib))
                 #self.lCurServos[self.iCurJoint].tCalib = self.tCurCalib
             self.iCurJoint = (self.iCurJoint+1)%3
             self.tCurCalib = self.lCurServos[self.iCurJoint].tCalib
             self.fCurShift = self.lCurServos[self.iCurJoint].fShift
-            print('Joint: CURR {} calib ({:.2f},{:.2f}) shift {:.2f}'.format(['hip','femur','tibia'][self.iCurJoint], self.tCurCalib[0], self.tCurCalib[1], self.fCurShift))
         def nextPosition(self):
             self.iCurPosition = (self.iCurPosition+1)%5
-            if self.iCurPosition == 0:
-                print('Pos: Femur up tibia right angle, ADJUST femur min/max')
-            elif self.iCurPosition == 1:
-                print('Pos: Flat horizontal leg, tibia 0° (no shift), ADJUST tibia min/max')
-            elif self.iCurPosition == 2:
-                print('Pos: Flat horizontal leg, tibia 0°, ADJUST tibia shift')
-            elif self.iCurPosition == 3:
-                print('Pos: Folded leg, tibia 180° (no shift), ADJUST tibia max/min value')
-            elif self.iCurPosition == 4:
-                print('Pos: Vertical leg DOWN, ADJUST femur max/min value')
         def updateAngles(self):
             fShift = self.lCurServos[2].fShift
             if self.iCurPosition in (1,3):
@@ -137,14 +111,67 @@ def xpadMenu(dev):
             self.fCurShift += vitShift
             self.lCurServos[self.iCurJoint].fShift = self.fCurShift
             self.updateAngles()
-        def printCalib(self):
-            print('Status: calib ({:.2f},{:.2f}) shift {:.2f}'.format(self.tCurCalib[0],
-                                                                      self.tCurCalib[1],
-                                                                      self.fCurShift))
         def activateAngles(self, bActive):
             self.bSetAngles = bActive
+        def refresh(self, fLatency):
+            STANDOUT = curses.A_BOLD|curses.A_UNDERLINE
+            stdscr.erase()
+            stdscr.move(0,0)
+            stdscr.addstr('Legs (current):  rear right left  middle right left  bottom right left')
+            stdscr.chgat(0,6, 7, STANDOUT)
+            for i in range(6):
+                stdscr.chgat(0, (22,28,41,47,60,66)[i], (5,4,5,4,5,4)[i],
+                             (i==self.iCurLeg and STANDOUT) or curses.A_NORMAL)
+            stdscr.addstr(1,0, 'Positions (current):')
+            stdscr.chgat(1,11, 7, STANDOUT)
+            stdscr.addstr(1,21, 'Femur up, tibia right angle, ADJUST femur min/max',
+                          self.iCurPosition==0 and STANDOUT or curses.A_NORMAL)
+            stdscr.addstr(2,21, 'Flat horizontal leg, tibia 0° (no shift), ADJUST tibia min/max',
+                          self.iCurPosition==1 and STANDOUT or curses.A_NORMAL)
+            stdscr.addstr(3,21, 'Flat horizontal leg, tibia 0°, ADJUST tibia shift',
+                          self.iCurPosition==2 and STANDOUT or curses.A_NORMAL)
+            stdscr.addstr(4,21, 'Folded leg, tibia 180° (no shift), ADJUST tibia max/min value',
+                          self.iCurPosition==3 and STANDOUT or curses.A_NORMAL)
+            stdscr.addstr(5,21, 'Vertical leg DOWN, ADJUST femur max/min value',
+                          self.iCurPosition==4 and STANDOUT or curses.A_NORMAL)
+            stdscr.addstr(6,0,'Joints (current):  hip  femur  tibia\n')
+            stdscr.chgat(6,8, 7, STANDOUT)
+            for i in range(3):
+                stdscr.chgat(6, (19, 24, 31)[i], (3,5,5)[i],
+                             (i==self.iCurJoint and STANDOUT) or curses.A_NORMAL)
+            stdscr.addstr(7,0,'Calibration:')
+            stdscr.addstr(7,13, 'min    {:6.0f}µs'.format(self.tCurCalib[0]))
+            stdscr.addstr(8,13, 'max    {:6.0f}µs'.format(self.tCurCalib[1]))
+            stdscr.addstr(9,13, 'shift  {:+5.2f}°'.format(self.fCurShift))
+            stdscr.addstr(10,0, 'Leg control: ')
+            if self.bSetAngles:
+                stdscr.addstr('ON', curses.A_REVERSE)
+            else:
+                stdscr.addstr('OFF', curses.A_REVERSE)
+            stdscr.addstr(12,0, '(latency: {: 4d}ms)'.format(int(fLatency*1000)))
+            stdscr.move(15,0)
+            stdscr.addstr('### HELP ###\n')
+            stdscr.addstr('  Start: quits\n')
+            stdscr.addstr('  Trigger right/left: next/prev leg\n')
+            stdscr.addstr('  A: cycles joints\n')
+            stdscr.addstr('  B: cycle leg positions\n')
+            stdscr.addstr('  Left Stick vertical: adjust min\n')
+            stdscr.addstr('  Right Stick vertical: adjust max\n')
+            stdscr.addstr('  Right Stick horizontal + right trigger axis pushed: adjust shift\n')
+            #stdscr.addstr('  Y: Show current calibration\n')
+            stdscr.addstr('  Left trigger axis: release to cancel control of the leg\n\n')
+            #stdscr.addstr('  X: this help')
+            stdscr.addstr('--- Calib order ---\n')
+            stdscr.addstr(' - Adjust femur up to be vertical\n')
+            stdscr.addstr(' - Adjust femur down to be vertical\n')
+            stdscr.addstr(' - Adjust tibia no shift unfolded (horizontal flat leg)\n')
+            stdscr.addstr(' - Adjust tibia no shift folded\n')
+            stdscr.addstr(' - Adjust tibia shift')
+            stdscr.move(13,0)
+            stdscr.refresh()
     status = Status()
     deadZone = lambda i,z: ((i<-z or i>z) and ( (i<-z and (i+z)) or (i>z and (i-z)) )) or 0
+    t0 = time.time()
     while not ecodes.BTN_START in dev.active_keys():
         # In fact, we must watch events...
         # Searches for falling edge of btns
@@ -172,6 +199,9 @@ def xpadMenu(dev):
                                 -deadZone(dAbsInput[ecodes.ABS_RY],5000)/100000)
         elif dAbsInput[ecodes.ABS_GAS] > 250:
             status.changeShift(deadZone(dAbsInput[ecodes.ABS_RX],5000)/1000000)
+        t = time.time()
+        status.refresh(t-t0)
+        t0 = t
         time.sleep(.01)
     sctl.setAngles([None]*18)
 
