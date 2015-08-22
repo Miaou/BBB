db93bccff5f637ed8f8df26dfad1b9ad80f3de4d
Miaou <miaou.pbl@gmail.com>
2015-08-22 23:57:08 +0200

    Trajectories with correct lengths

--- libServo/trajectory.py
+++ libServo/trajectory.py
@@ -6,14 +6,42 @@ from math import cos, sin, acos, atan, pi
 
 
 
-def quickTraj(Vx, Vy, Omega, u, Sx, Sy, r):
+def computeDeltaTheta(Vx, Vy, Omega, lSxy, r, deltaU):
     '''
     Vx and Vy are planar velocities of the object, in mm/s
-    Omega is the abgular velocity of the object, in rad/s
+    Omega is the angular velocity of the object, in rad/s
+    lSxy is the list of all leg's sweet spot (Sx,Sy), "center" of the trajectory
+    r is the radius of reachable positions from S, in mm
+    deltaU is half the shortest time to take a leg from -r to +r (fastest movement),
+     and deltaTheta is computed so that a leg step foot exactly 2*deltaU
+     (speed is reduced if it is not possible)
+    Returns deltaTheta in rad. OmegaMax is deltaTheta/deltaU. Max velocity is r/deltaU.
+    '''
+    if Omega != 0:
+        lDT = []
+        for Sx,Sy in lSxy:
+            x = +Vy+Omega*Sx
+            y = -Vx+Omega*Sy
+            R = (x**2 + y**2)**.5/abs(Omega)
+            try:
+                lDT.append( acos((2*R**2-r**2)/(2*R**2)) )
+            except ValueError:
+                lDT.append( pi )
+        return min(min(( Omega*deltaU, deltaTheta )) for deltaTheta in lDT)
+    else:
+        # This is not exactly deltaTheta, but at least it is comparable to rad
+        return min( ((Vx**2+Vy**2)**.5*deltaU/r, 1) )
+
+    
+def quickTraj(Vx, Vy, Omega, u, Sx, Sy, r, deltaTheta):
+    '''
+    Vx and Vy are planar velocities of the object, in mm/s
+    Omega is the angular velocity of the object, in rad/s
     u is in [-1..1]
     The trajectory goes through (Sx, Sy) at u=0, in mm
     r is the radius of reachable positions from S, in mm
-    r must be the same for all legs
+    deltaTheta is the max angular amplitude along the trajectory, in rad
+    r and deltaTheta must be the same for all legs
     Returns (x(u), y(u), z(u)=0)
     '''
     x = +Vy+Omega*Sx
@@ -21,24 +49,22 @@ def quickTraj(Vx, Vy, Omega, u, Sx, Sy, r):
     Theta0 =  (atan( y/x ) if x!= 0 else (y>0 and pi/2 or -pi/2)) + (pi if x*(Omega or 1.)<0 else 0)
     if Omega != 0:
         R = (x**2 + y**2)**.5/abs(Omega)
-        try:
-            deltaTheta = acos( (2*R**2 - r**2)/(2*R**2) ) # deltaTheta should be the same for all legs, which is not the case...
-        except ValueError:
-            deltaTheta = pi
         return (R*(cos(Theta0-u*deltaTheta) - cos(Theta0)), # x(u)
                 R*(sin(Theta0-u*deltaTheta) - sin(Theta0)), # y(u)
                 0) # z(u)
     else:
-        return (+r*u*sin(Theta0), # Yes, using Taylor expansion of the above
-                -r*u*cos(Theta0),
+        return (+r*u*deltaTheta*sin(Theta0), # Yes, using Taylor expansion of the above
+                -r*u*deltaTheta*cos(Theta0),
                 0)
 
 
 def plotTraj(Vx,Vy,Omega,sCol='b'):
-    for Sx,Sy in ((-42.75, -82.5), (+42.75, -82.5),
-                  (-63,0),         (+63,0),
-                  (-42.75, +82.5), (+42.75, +82.5)):
-        lX,lY,lZ = list(zip(*(quickTraj(Vx, Vy, Omega, u, Sx, Sy, 30) for u in pylab.r_[-1:1:11j])))
+    lSxy = ((-42.75, -82.5), (+42.75, -82.5),
+            (-63,0),         (+63,0),
+            (-42.75, +82.5), (+42.75, +82.5))
+    deltaTheta = computeDeltaTheta(Vx, Vy, Omega, lSxy, 30, 1)
+    for Sx,Sy in lSxy:
+        lX,lY,lZ = list(zip(*(quickTraj(Vx, Vy, Omega, u, Sx, Sy, 30, deltaTheta) for u in pylab.r_[-1:1:11j])))
         pylab.plot((Sx+lX[0],),(Sy+lY[0],),sCol+'x')
         pylab.plot([Sx+x for x in lX],[Sy+y for y in lY],sCol+'-')
         pylab.plot((Sx,),(Sy,),'kx')
