fcd847f5de1b395e18e87fa1e6ea56740f5b6e40
Miaou <miaou.pbl@gmail.com>
2015-08-25 13:27:47 +0200

    Using xpad: separate commons

--- libServo/calibXBox.py
+++ libServo/calibXBox.py
@@ -4,48 +4,11 @@
 # Small tool provided more than anything else "as-is", just for fun...
 
 
-from evdev import InputDevice, ecodes
 from config import lServos
 from servos import PruInterface, ServoController
 import time
+from joy import dev, ecodes, getAxesValues
 import curses
-
-
-# InputDevice has a nice active_keys() which avoids the parsing of the events, but not abs_values()...
-# This should not be THAT hard to add. However, I don't want to compile/test/install, and not very comfortable with the Python C-API.
-# To do so, see https://github.com/gvalkov/python-evdev/blob/master/evdev/device.py,
-#  -> active_keys() uses _input.ioctl_EVIOCGKEY(self.fd) (see https://github.com/gvalkov/python-evdev/blob/master/evdev/input.c)
-#   and implementing a ioctl_EVIOCGABS should not be that hard
-#  -> but abs values are also stored in the capabilities() (see device.py)
-#   and this is generated when ._rawcapabilities is created,
-#   and this is done by _input.ioctl_capabilities() (see input.c)
-#   which has the code ready for EVIOCGABS
-#  -> so we just have to call _input.ioctl_capabilities() and find the interesting value.
-#   (our task is not real-time sensitive)
-
-from evdev._input import ioctl_capabilities # Sole parameter: InputDevice.fd
-
-
-
-
-
-def getAxesValues(dev):
-    # Yes, this is "kind of" brutal, but who cares...
-    # 1) Get _rawcapabilities (via ioctl_[...])
-    # 2) Take EV_ABS part out of it, which is a list [(iAxis, tAbsInfo), ...]
-    # 3) Reform the list as a more usable dict
-    return {iAxis:tAbsInfo[0] for iAxis,tAbsInfo in ioctl_capabilities(dev.fd)[ecodes.EV_ABS]} # In fact, we don't need InputDevice
-
-
-def testRawCapa():
-    POLL_PERIOD = .5
-    t0 = time.time()
-    while True:
-        if time.time()-t0 > POLL_PERIOD:
-            dAbsInputs = getAxesValues(dev)
-            print('  '.join('{}: {:<6}'.format(ecodes.ABS[k][4:],v) for k,v in dAbsInputs.items()))
-            t0 += POLL_PERIOD
-        time.sleep(.01)
 
 
 
---
+++ libServo/joy.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+#
+
+
+from evdev import InputDevice, ecodes
+import time
+import curses
+
+# InputDevice has a nice active_keys() which avoids the parsing of the events, but not abs_values()...
+# This should not be THAT hard to add. However, I don't want to compile/test/install, and not very comfortable with the Python C-API.
+# To do so, see https://github.com/gvalkov/python-evdev/blob/master/evdev/device.py,
+#  -> active_keys() uses _input.ioctl_EVIOCGKEY(self.fd) (see https://github.com/gvalkov/python-evdev/blob/master/evdev/input.c)
+#   and implementing a ioctl_EVIOCGABS should not be that hard
+#  -> but abs values are also stored in the capabilities() (see device.py)
+#   and this is generated when ._rawcapabilities is created,
+#   and this is done by _input.ioctl_capabilities() (see input.c)
+#   which has the code ready for EVIOCGABS
+#  -> so we just have to call _input.ioctl_capabilities() and find the interesting value.
+#   (our task is not real-time sensitive, and the overhead is not that much)
+
+from evdev._input import ioctl_capabilities # Sole parameter: InputDevice.fd
+
+def getAxesValues(dev):
+    # Yes, this is "kind of" brutal, but who cares...
+    # 1) Get _rawcapabilities (via ioctl_[...])
+    # 2) Take EV_ABS part out of it, which is a list [(iAxis, tAbsInfo), ...]
+    # 3) Reform the list as a more usable dict
+    return {iAxis:tAbsInfo[0] for iAxis,tAbsInfo in ioctl_capabilities(dev.fd)[ecodes.EV_ABS]} # In fact, we don't need InputDevice
+
+
+
+try:
+    dev = InputDevice('/dev/input/event1') # Hardcoded, but, hey, it's only for the BBB
+except OSError as e:
+    print('----> Did you run xboxdrv? <----')
+    raise e
+
+
+if __name__=='__main__':
+    # Old stuff
+    def testRawCapa():
+        POLL_PERIOD = .5
+        t0 = time.time()
+        while True:
+            if time.time()-t0 > POLL_PERIOD:
+                dAbsInputs = getAxesValues(dev)
+                print('  '.join('{}: {:<6}'.format(ecodes.ABS[k][4:],v) for k,v in dAbsInputs.items()))
+                t0 += POLL_PERIOD
+            time.sleep(.01)
+    
+    def displayJoy(stdscr, dev):
+        stdscr.addstr(0,0,'--> Start to Quit <--')
+        while not ecodes.BTN_START in dev.active_keys():
+            dAbsInput = getAxesValues(dev)
+            stdscr.move(1,0)
+            stdscr.addstr('Active btns: '+' '.join(str(ecodes.BTN[i]) for i in dev.active_keys())+'\n')
+            stdscr.addstr('Axes values:\n')
+            for i,f in dAbsInput.items():
+                stdscr.addstr('  {}: {}\n'.format(ecodes.ABS[i],f))
+            stdscr.refresh()
+            time.sleep(.1)
+
+    curses.wrapper(displayJoy, dev)
